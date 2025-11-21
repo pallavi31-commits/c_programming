d7cfbda4707a4373cb14ae813e23c8020c11fdc3
Pallavi Govind Rao <govindraopallavi@gmail.com>
2025-11-21 18:09:15 +0530

    Update 1oddEven.c

--- if_else/1oddEven.c
+++ if_else/1oddEven.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main(){
+    int n;  //dabba ban gaya
+    printf("Enter a number :- ");   //output me aagayi
+    scanf("%d",&n);   //dabbe me value daal di
+    if(n%2==0){      //even condition
+        printf("even number");
+    }
+    // if(n%2!=0){     //odd condition
+    // printf("odd number");
+    // }
+    else{
+        printf("odd number");
+    }
+    return 0;
+}
